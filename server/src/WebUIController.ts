3525485d82300e6b566a73a188d500ebcbcc3d24
noname <48761044+noname0310@users.noreply.github.com>
2022-10-13 00:44:23 +0900

    Update WebUIController.ts

--- server/src/WebUIController.ts
+++ server/src/WebUIController.ts
@@ -110,6 +110,8 @@ export class WebUIController {
         return false;
     }
 
+    lastImage: string|null = null;
+
     public async flushImages(): Promise<string[]|null> {
         if (!this._processing) return null;
 
@@ -119,9 +121,13 @@ export class WebUIController {
         const images = await gallery.$$("img");
         if (images.length === 0) return null;
 
+        if (await images[0].evaluate(element => element.src) === this.lastImage) return null;
+
         const base64ImageUrls = await Promise.all(images.map(async image => {
             return await image.evaluate(image => image.src);
         }));
+
+        this.lastImage = base64ImageUrls[0];
 
         this._processing = false;
         return base64ImageUrls;
