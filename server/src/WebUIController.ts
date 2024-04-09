ed35ca15d602487273aaedb6cd6e5f5509fa03e0
noname <48761044+noname0310@users.noreply.github.com>
2024-04-09 11:36:14 +0900

    Update WebUIController.ts

--- server/src/WebUIController.ts
+++ server/src/WebUIController.ts
@@ -2,32 +2,29 @@ import * as puppeteer from "puppeteer";
 
 export class WebUIController {
     private readonly _browser: puppeteer.Browser;
-    private readonly _shadowRoot: puppeteer.ElementHandle<ShadowRoot>;
+    private readonly _page: puppeteer.Page;
     private _generateBlock = false;
     private _processing = false;
 
     private constructor(
         browser: puppeteer.Browser,
-        shadowRoot: puppeteer.ElementHandle<ShadowRoot>
+        page: puppeteer.Page
     ) {
         this._browser = browser;
-        this._shadowRoot = shadowRoot;
+        this._page = page;
     }
 
     public static async create(): Promise<WebUIController> {
         const browser = await puppeteer.launch({
-            headless: true,
+            headless: false,
             defaultViewport: null
         });
         const page = (await browser.pages())[0];
         await page.goto("http://127.0.0.1:7860");
-
-        const shadowRoot = await page.evaluateHandle(() => document.querySelector("body > gradio-app")?.shadowRoot ?? null);
-        if (shadowRoot.jsonValue() === null) throw new Error("Failed to get shadowRoot");
-
+        
         return new WebUIController(
             browser,
-            shadowRoot as puppeteer.ElementHandle<ShadowRoot>
+            page
         );
     }
 
@@ -36,8 +33,10 @@ export class WebUIController {
     }
 
     public async isGenerating(): Promise<boolean> {
-        const element = await this._shadowRoot.$("#txt2img_interrupt") as puppeteer.ElementHandle<HTMLButtonElement>;
+        const element = await this._page.$("#txt2img_interrupt") as puppeteer.ElementHandle<HTMLButtonElement>;
         if (element === null) return false;
+
+        console.log("element", element);
 
         let displayProperty = await element.evaluate(element => element.style.display);
         if (displayProperty === "") displayProperty = "none";
@@ -53,24 +52,31 @@ export class WebUIController {
             : promptText;
         
         const negativePromptTexts = [
-            "nsfw",
-            "lowres",
-            "bad anatomy",
-            "bad hands",
+            "(worst quality:1.4)",
+            "(low quality:1.4)",
             "text",
-            "error",
-            "missing fingers",
-            "extra digit",
-            "fewer digits",
-            "cropped",
-            "worst quality",
-            "low quality",
-            "normal quality",
-            "jpeg artifacts",
-            "signature",
-            "watermark",
-            "username",
-            "blurry"
+            "grid view",
+            "nsfw",
+            "crumpled limbs",
+            "Bad Anatomy"
+            // "nsfw",
+            // "lowres",
+            // "bad anatomy",
+            // "bad hands",
+            // "text",
+            // "error",
+            // "missing fingers",
+            // "extra digit",
+            // "fewer digits",
+            // "cropped",
+            // "worst quality",
+            // "low quality",
+            // "normal quality",
+            // "jpeg artifacts",
+            // "signature",
+            // "watermark",
+            // "username",
+            // "blurry"
         ].join(", ");
 
         if (await this.isGenerating()) {
@@ -78,7 +84,7 @@ export class WebUIController {
             return false;
         }
 
-        const promptDiv = await this._shadowRoot.$("#txt2img_prompt");
+        const promptDiv = await this._page.$("#txt2img_prompt");
         if (promptDiv === null) {
             this._generateBlock = false;
             return false;
@@ -87,7 +93,7 @@ export class WebUIController {
             element.value = value;
             element.dispatchEvent(new Event("input", {bubbles:true}));
         }, promptText);
-        const negPromptDiv = await this._shadowRoot.$("#negative_prompt");
+        const negPromptDiv = await this._page.$("#txt2img_neg_prompt");
         if (negPromptDiv === null) {
             this._generateBlock = false;
             return false;
@@ -97,7 +103,7 @@ export class WebUIController {
             element.dispatchEvent(new Event("input", {bubbles:true}));
         }, negativePromptTexts);
 
-        const generateButton = await this._shadowRoot.$("#txt2img_generate");
+        const generateButton = await this._page.$("#txt2img_generate");
         if (generateButton) {
             await generateButton.click();
             this._processing = true;
@@ -113,23 +119,32 @@ export class WebUIController {
     private _lastImage: string|null = null;
 
     public async flushImages(): Promise<string[]|null> {
-        if (!this._processing) return null;
-
-        const gallery = await this._shadowRoot.$("#txt2img_gallery");
+        this._processing;
+        const gallery = await this._page.$("#txt2img_gallery");
         if (gallery === null) return null;
 
         const images = await gallery.$$("img");
         if (images.length === 0) return null;
 
-        if (await images[0].evaluate(element => element.src) === this._lastImage) return null;
+        if (await images[0].evaluate(element => element.src) === this._lastImage) {
+            console.log(this._lastImage);
+            return null;
+        }
 
-        const base64ImageUrls = await Promise.all(images.map(async image => {
+        const localImageUrls = await Promise.all(images.map(async image => {
             return await image.evaluate(image => image.src);
         }));
 
-        this._lastImage = base64ImageUrls[0];
+        this._lastImage = localImageUrls[0];
+
+        const base64Images = await Promise.all(localImageUrls.map(async url => {
+            const response = await fetch(url);
+            const buffer = await response.arrayBuffer();
+            const base64 = Buffer.from(buffer).toString("base64");
+            return "data:image/png;base64," + base64;
+        }));
 
         this._processing = false;
-        return base64ImageUrls;
+        return base64Images;
     }
 }
